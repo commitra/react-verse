848b11c2fe6259631ef1b6ec56ee2d79e452be9e
Codesneha <sneha21030@gmail.com>
2025-10-16 01:38:17 +0530

    UI Home Page

---
+++ src/pages/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="dashboard-header">
+      <div className="header-content">
+        <h1 className="dashboard-title">Dashboard Hub</h1>
+        <p className="dashboard-subtitle">
+          Your all-in-one portal for real-time data, tools, and fun.
+        </p>
+      </div>
+    </header>
+  );
+}
